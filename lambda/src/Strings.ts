6f5534dcdc6ade8f775edb56fab9445db640c64b
Andrew Reynolds <areynold@illinois.edu>
2020-03-31 16:42:42 -0500

    correct expected strings

--- lambda/src/Strings.ts
+++ lambda/src/Strings.ts
@@ -2,11 +2,11 @@
 
 let EnglishStrings = {
     "WELCOME_MSG": "Welcome to {0}",
-    "HELP_MSG": "Welcome to {0}. You can play, stop, resume listening.  How can I help you ?",
+    "HELP_MSG": "Welcome to {0}. You can play, stop, resume listening. How can I help you ?",
     "UNHANDLED_MSG": "Sorry, I could not understand what you've just said.",
     "CAN_NOT_SKIP_MSG": "This is radio, you have to wait for next track to play.",
     "RESUME_MSG": "Resuming {0}",
-    "NOT_POSSIBLE_MSG": "This is radio, you can not do that.  You can ask me to stop or pause to stop listening.",
+    "NOT_POSSIBLE_MSG": "This is radio, you can not do that. You can ask me to stop or pause to stop listening.",
     "STOP_MSG": "Goodbye.",
     "DEVICE_NOT_SUPPORTED": "Sorry, this skill is not supported on this device",
 
